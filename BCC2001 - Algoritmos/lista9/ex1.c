d4a4452df2edaf84b8fc3740a453e75a87abcdbb
ChristoferLv <christofer_daniel12@hotmail.com>
2025-04-10 17:41:39 -0300

    finalizada lsita 9

--- BCC2001 - Algoritmos/lista9/ex1.c
+++ BCC2001 - Algoritmos/lista9/ex1.c
@@ -2,6 +2,19 @@
 #include <string.h>
 #include <limits.h>
 
+struct rect
+{
+    int x, y, width, height; // é possível declarar os campos em uma só linha
+};
+typedef struct rect Rect;
+
+struct point
+{
+    int x, y; // é possível declarar os campos em uma só linha
+    char symbol;
+};
+typedef struct point Point;
+
 void print_addrs() // Ex1
 {
     char v1;
@@ -94,6 +107,107 @@ int get_hamming_distance(const char *str1, const char *str2) // Ex8
     return dist;
 }
 
+char *find_most_similar(const char *str, int n, char list[n][50])
+{
+    size_t most = INT_MAX;
+    size_t pos = 0;
+    size_t temp = 0;
+    for (size_t i = 0; i < n; i++)
+    {
+        temp = get_hamming_distance(list[i], str);
+        if (temp < most)
+        {
+            most = temp;
+            pos = i;
+        }
+    }
+    return list[pos];
+}
+
+void print_board(Rect *board)
+{
+    for (size_t i = 0; i < board->y; i++)
+    {
+        printf("\n");
+    }
+    for (size_t i = 0; i < board->height; i++)
+    {
+        for (size_t j = 0; j < board->x; j++)
+        {
+            printf(" ");
+        }
+        for (size_t j = 0; j < board->width; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void print_board2(Point *point, Rect *board)
+{
+    for (size_t i = 0; i < board->y; i++)
+    {
+        printf("\n");
+    }
+    for (size_t i = 0; i < board->height; i++)
+    {
+        for (size_t j = 0; j < board->x; j++)
+        {
+            printf(" ");
+        }
+        for (size_t j = 0; j < board->width; j++)
+        {
+            if (i == point->y && j == point->x)
+            {
+                printf("%c", point->symbol);
+            }
+            else
+            {
+                printf("#");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void print_board3(Point points[], size_t size, Rect *board)
+{
+    char buffer[board->height][board->width];
+    for (size_t i = 0; i < board->height; i++)
+    {
+        for (size_t j = 0; j < board->width; j++)
+        {
+            buffer[i][j] = '#';
+        }
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        buffer[points[i].x][points[i].y] = points[i].symbol;
+    }
+
+    for (size_t i = 0; i < board->y; i++)
+    {
+        printf("\n");
+    }
+    for (size_t i = 0; i < board->height; i++)
+    {
+        for (size_t j = 0; j < board->x; j++)
+        {
+            printf(" ");
+        }
+        for (size_t j = 0; j < board->width; j++)
+        {
+            printf("%c", buffer[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
 int main()
 {
     print_addrs();
@@ -125,5 +239,25 @@ int main()
 
     int dist = get_hamming_distance("banana", "cabana");
     printf("Distance: %d\n", dist);
+
+    char words[][50] = {"cabana", "savana", "bacana", "halana"};
+    char *most_similar = find_most_similar("banana", 4, words);
+    printf("%s\n", most_similar); // "bacana"
+
+    printf("_____________________________________________________\n");
+    Rect rect = {5, 5, 10, 10}; // posição 5,5 (x,y) e dimensões 10x10 (width,height)
+    print_board(&rect);
+
+    printf("_____________________________________________________\n");
+    Rect rect2 = {5, 5, 10, 10}; // posição 5,5 (x,y) e dimensões 10x10 (width,height)
+    Point point = {2, 4, 'A'};   // posição 0,0 dentro do tablado
+    print_board2(&point, &rect2);
+
+    printf("_____________________________________________________\n");
+    Rect rect3 = {5, 5, 10, 10}; // posição 5,5 (x,y) e dimensões 10x10 (width,height)
+    Point points2[] = {{0, 0, 'A'}, {2, 7, 'B'}, {9, 9, 'C'}};
+    size_t size = 3;
+    print_board3(points2, size, &rect3);
+
     return 0;
 }
